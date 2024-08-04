20c0387f39bc00d2377ef93a479eb7b7465aac28
BySkullZ <bahrimomo061@gmail.com>
2024-08-04 17:56:45 +0200

    Hotfix monster health

--- Assets/Scripts/Online/HitboxManager.cs
+++ Assets/Scripts/Online/HitboxManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class HitboxManager : NetworkBehaviour
 {
+    [SyncVar]
     private List<GameObject> debounce;
     // Start is called before the first frame update
     void Start()
